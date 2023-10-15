c7c1c0cf41c1aa626b659749a5eef4f4fee8df0e
Damian Vela Palacios <damianvelapal@gmail.com>
2023-10-15 22:17:32 +0200

    bug

--- src/main/java/domain/Quote.java
+++ src/main/java/domain/Quote.java
@@ -91,7 +91,7 @@ public class Quote implements Serializable {
 	public void removeBet(Bet b) {
 		for (int i=0; i<this.bets.size(); i++) {
 			Bet bet = this.bets.get(i);
-			if(bet.getBetNumber()==b.getBetNumber()) {
+			if(bet.getBetNumber().equals(b.getBetNumber())) {
 				this.bets.remove(i);
 			}
 		}
