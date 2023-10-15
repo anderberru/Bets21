c727e7260dcc8bea0fc7e880c8635e7614110e16
Damian Vela Palacios <damianvelapal@gmail.com>
2023-10-15 22:05:09 +0200

    smel3

--- src/main/java/configuration/ConfigXML.java
+++ src/main/java/configuration/ConfigXML.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.stream.XMLInputFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
